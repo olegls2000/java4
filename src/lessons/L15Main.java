a6ef427e222a61fca08d134c338f6e99540d3de0
hala1dala <oleg.malahhov@gmail.com>
2021-02-10 18:40:53 +0200

    Added to my work

--- src/lessons/L15Main.java
+++ src/lessons/L15Main.java
@@ -7,14 +7,26 @@ import processor.NumberCheckProcessor;
 public class L15Main {
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        NumberCheckProcessor numberCheckProcessor = NumberCheckProcessor.getInstance();
+
+
         Circle circle = new Circle();
-        NumberCheckProcessor numberCheckProcessor = new NumberCheckProcessor();
+        //circle.setSide(0);
         numberCheckProcessor.process(circle);
 
-
         CargoCar car = new CargoCar();
+        car.setYear(2000);
         numberCheckProcessor.process(car);
 
+        long stop = System.currentTimeMillis();
+        long duration = stop - start;
+        System.out.println("Time = " + duration + " msec");
+        processSmth();
+    }
 
+    private static void processSmth() {
+        NumberCheckProcessor processor = NumberCheckProcessor.getInstance();
+        processor.process(null);
     }
 }
